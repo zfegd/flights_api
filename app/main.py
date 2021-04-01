7b95ffd251acfda89d8f38193a0aa6d227538b7d
Ash <soccer1996ash@yahoo.com.sg>
2021-04-01 08:59:09 +0800

    Changed dockerfile arch to from python and install uvicorn

--- app/main.py
+++ app/main.py
@@ -4,10 +4,10 @@ from fastapi import FastAPI
 # import pandas as pd
 
 app = FastAPI()
-#
-# @app.get("/")
-# def read_root():
-#     return {"Hello": "World"}
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 #
 #
 # @app.get("/items/{item_id}")
@@ -15,33 +15,33 @@ app = FastAPI()
 #     return {"item_id": item_id, "q": q}
 
 
-@app.get("/airport/{airport_name}")
-def get_airport_details(airport_name: str):
-    df = fp.load_unto_dataframe()
-    relevant = df[df["Name"].str.contains(airport_name)]
-    # convert relevant dataframe to json object
-    return None
-
-@app.get("/country/{country_name}")
-def get_country_airports(country_name: str):
-    df = fp.load_unto_dataframe()
-    relevant = df[df["Country"] == country_name]
-    # convert relevant dataframe to json object
-    return None
-
-@app.get("/city/{city_name}")
-def get_city_airports(city_name: str):
-    df = fp.load_unto_dataframe()
-    relevant = df[df["City"] == city_name]
-    # convert relevant dataframe to json object
-    return None
-
-@app.get("/timezone/{time_zone}")
-def get_airports_within_timezone(time_zone : int):
-    df = fp.load_unto_dataframe()
-    relevant = df[df["Timezone"] == str(time_zone)]
-    # convert relevant dataframe to json object
-    return None
+# @app.get("/airport/{airport_name}")
+# def get_airport_details(airport_name: str):
+#     df = fp.load_unto_dataframe()
+#     relevant = df[df["Name"].str.contains(airport_name)]
+#     # convert relevant dataframe to json object
+#     return None
+#
+# @app.get("/country/{country_name}")
+# def get_country_airports(country_name: str):
+#     df = fp.load_unto_dataframe()
+#     relevant = df[df["Country"] == country_name]
+#     # convert relevant dataframe to json object
+#     return None
+#
+# @app.get("/city/{city_name}")
+# def get_city_airports(city_name: str):
+#     df = fp.load_unto_dataframe()
+#     relevant = df[df["City"] == city_name]
+#     # convert relevant dataframe to json object
+#     return None
+#
+# @app.get("/timezone/{time_zone}")
+# def get_airports_within_timezone(time_zone : int):
+#     df = fp.load_unto_dataframe()
+#     relevant = df[df["Timezone"] == str(time_zone)]
+#     # convert relevant dataframe to json object
+#     return None
 
 # def get_airport_within_geobox():
 #     return None
