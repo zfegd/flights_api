64f42ea2f241ff10b54523abac81db7c3c6ab7aa
Ash <soccer1996ash@yahoo.com.sg>
2021-04-01 09:23:48 +0800

    Refactored load dataframe into main app, not sure about file routing

--- app/main.py
+++ app/main.py
@@ -1,44 +1,46 @@
 from typing import Optional
 from fastapi import FastAPI
-# import fileparser as fp
-# import pandas as pd
+import pandas as pd
 
 app = FastAPI()
+
+def load_unto_dataframe():
+    myheaders = ["Airport ID","Name","City","Country","IATA","ICAO","Latitude",
+        "Longitude", "Altitude", "Timezone", "DST", "TZ", "Type", "Source"]
+    df = pd.read_csv("data/airports.dat", names=myheaders)
+    df = df.replace("\\N", "Not Found")
+    # resolve issue where can't be displayed - name,city (need to encode to UTF8)
+    # issue due to polish letters
+    return df
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-#
-#
-# @app.get("/items/{item_id}")
-# def read_item(item_id: int, q: Optional[str] = None):
-#     return {"item_id": item_id, "q": q}
-
 
 # @app.get("/airport/{airport_name}")
 # def get_airport_details(airport_name: str):
-#     df = fp.load_unto_dataframe()
+#     df = load_unto_dataframe()
 #     relevant = df[df["Name"].str.contains(airport_name)]
 #     # convert relevant dataframe to json object
 #     return None
 #
 # @app.get("/country/{country_name}")
 # def get_country_airports(country_name: str):
-#     df = fp.load_unto_dataframe()
+#     df = load_unto_dataframe()
 #     relevant = df[df["Country"] == country_name]
 #     # convert relevant dataframe to json object
 #     return None
 #
 # @app.get("/city/{city_name}")
 # def get_city_airports(city_name: str):
-#     df = fp.load_unto_dataframe()
+#     df = load_unto_dataframe()
 #     relevant = df[df["City"] == city_name]
 #     # convert relevant dataframe to json object
 #     return None
 #
 # @app.get("/timezone/{time_zone}")
 # def get_airports_within_timezone(time_zone : int):
-#     df = fp.load_unto_dataframe()
+#     df = load_unto_dataframe()
 #     relevant = df[df["Timezone"] == str(time_zone)]
 #     # convert relevant dataframe to json object
 #     return None
