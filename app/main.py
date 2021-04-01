db670f33af7b3812fd1edad78a70ae04ee1c1fa5
Ash <soccer1996ash@yahoo.com.sg>
2021-04-01 10:06:02 +0800

    Basic infra done without database, to consider if JSON format is neat enough and if language error is a priority to fix

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ import pandas as pd
 app = FastAPI()
 
 def load_unto_dataframe():
-    myheaders = ["Airport ID","Name","City","Country","IATA","ICAO","Latitude",
+    myheaders = ["AirportID","Name","City","Country","IATA","ICAO","Latitude",
         "Longitude", "Altitude", "Timezone", "DST", "TZ", "Type", "Source"]
     df = pd.read_csv("data/airports.dat", names=myheaders)
     df = df.replace("\\N", "Not Found")
@@ -13,46 +13,33 @@ def load_unto_dataframe():
     # issue due to polish letters
     return df
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-@app.get("/df/")
-def test_df():
-    df = load_unto_dataframe()
-    size = df.shape[0]
-    return {"Data": size}
 
 @app.get("/airport/{airport_name}")
 def get_airport_details(airport_name: str):
     df = load_unto_dataframe()
     df["Name"] = df["Name"].str.lower()
     relevant = df[df["Name"].str.contains(airport_name.lower())]
-    # convert relevant dataframe to json object
-    count = relevant.shape[0]
-    return {"Airports":count}
+    return relevant.to_json()
 
-#
-# @app.get("/country/{country_name}")
-# def get_country_airports(country_name: str):
-#     df = load_unto_dataframe()
-#     relevant = df[df["Country"] == country_name]
-#     # convert relevant dataframe to json object
-#     return None
-#
-# @app.get("/city/{city_name}")
-# def get_city_airports(city_name: str):
-#     df = load_unto_dataframe()
-#     relevant = df[df["City"] == city_name]
-#     # convert relevant dataframe to json object
-#     return None
-#
-# @app.get("/timezone/{time_zone}")
-# def get_airports_within_timezone(time_zone : int):
-#     df = load_unto_dataframe()
-#     relevant = df[df["Timezone"] == str(time_zone)]
-#     # convert relevant dataframe to json object
-#     return None
+@app.get("/country/{country_name}")
+def get_country_airports(country_name: str):
+    df = load_unto_dataframe()
+    df["Country"] = df["Country"].str.lower()
+    relevant = df[df["Country"] == country_name.lower()]
+    return relevant.to_json()
+
+@app.get("/city/{city_name}")
+def get_city_airports(city_name: str):
+    df = load_unto_dataframe()
+    df["City"] = df["City"].str.lower()
+    relevant = df[df["City"] == city_name.lower()]
+    return relevant.to_json()
+
+@app.get("/timezone/{time_zone}")
+def get_airports_within_timezone(time_zone : int):
+    df = load_unto_dataframe()
+    relevant = df[df["Timezone"] == str(time_zone)]
+    return relevant.to_json()
 
 # def get_airport_within_geobox():
 #     return None
