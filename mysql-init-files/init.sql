39c4b850deb1fe964d989e2260c359cfd2dbdce0
Ash <soccer1996ash@yahoo.com.sg>
2021-04-08 16:51:45 +0800

    managed mysql folders

--- mysql-init-files/init.sql
+++ mysql-init-files/init.sql
@@ -1,6 +1,4 @@
-CREATE DATABASE OpenFlights;
-
-CREATE TABLE OpenFlights.Airports(
+CREATE TABLE Airports(
   AirportID int,
   Name varchar(255),
   City varchar(255),
@@ -17,6 +15,9 @@ CREATE TABLE OpenFlights.Airports(
   Source varchar(255)
 );
 
-LOAD DATA INFILE "../data/airports.dat"
-INTO TABLE Airports
-FIELDS TERMINATED BY ',';
+INSERT INTO Airports
+VALUES (1,"Goroka Airport","Goroka","Papua New Guinea","GKA","AYGA",-6.081689834590001,145.391998291,5282,10,"U","Pacific/Port_Moresby","airport","OurAirports");
+
+-- LOAD DATA INFILE "../data/airports.dat"
+-- INTO TABLE Airports
+-- FIELDS TERMINATED BY ',';
