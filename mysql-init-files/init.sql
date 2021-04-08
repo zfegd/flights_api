2df1e5c7bcdc499c0e4697c7e9bebb5d97bc65ba
Ash <soccer1996ash@yahoo.com.sg>
2021-04-08 16:58:03 +0800

    insert into database works, to try with file

--- mysql-init-files/init.sql
+++ mysql-init-files/init.sql
@@ -15,9 +15,9 @@ CREATE TABLE Airports(
   Source varchar(255)
 );
 
-INSERT INTO Airports
-VALUES (1,"Goroka Airport","Goroka","Papua New Guinea","GKA","AYGA",-6.081689834590001,145.391998291,5282,10,"U","Pacific/Port_Moresby","airport","OurAirports");
+-- INSERT INTO Airports
+-- VALUES (1,"Goroka Airport","Goroka","Papua New Guinea","GKA","AYGA",-6.081689834590001,145.391998291,5282,10,"U","Pacific/Port_Moresby","airport","OurAirports");
 
--- LOAD DATA INFILE "../data/airports.dat"
--- INTO TABLE Airports
--- FIELDS TERMINATED BY ',';
+LOAD DATA INFILE "../data/airports.dat"
+INTO TABLE Airports
+FIELDS TERMINATED BY ',';
