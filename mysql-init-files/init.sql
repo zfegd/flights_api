68996d3edfa93be25f5f306b58c7267498be2b32
Ash <soccer1996ash@yahoo.com.sg>
2021-04-09 09:37:26 +0800

    fixed permissions issue and volume mounting issue, next to fix is finding file

--- mysql-init-files/init.sql
+++ mysql-init-files/init.sql
@@ -18,6 +18,8 @@ CREATE TABLE Airports(
 -- INSERT INTO Airports
 -- VALUES (1,"Goroka Airport","Goroka","Papua New Guinea","GKA","AYGA",-6.081689834590001,145.391998291,5282,10,"U","Pacific/Port_Moresby","airport","OurAirports");
 
-LOAD DATA LOCAL INFILE "../app/data/airports.dat"
+
+LOAD DATA INFILE "../../../../data/air2.dat"
 INTO TABLE Airports
-FIELDS TERMINATED BY ',';
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n';
