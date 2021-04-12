f2cb3cea51e24881a4288e7311c368c32470f4d5
Ash <soccer1996ash@yahoo.com.sg>
2021-04-12 17:31:56 +0800

    locust mini tests works locally

--- app/load_test.py
+++ app/load_test.py
@@ -1,7 +1,10 @@
-from locust import HttpUser, TaskSet, task
+from locust import HttpUser, between, task
 
 
-class UserBehavior(TaskSet):
+class MyUser(HttpUser):
+
+    wait_time = between(5, 15)
+    host = 'http://127.0.0.1'
 
     @task
     def get_airport_correct(self):
@@ -20,8 +23,27 @@ class UserBehavior(TaskSet):
         self.client.get("/v0.1/airport?airport_name=")
 
 
-class WebsiteUser(HttpUser):
-    task_set = UserBehavior
-    host = 'http://127.0.0.1'
-    min_wait = 5000
-    max_wait = 9000
+# class UserBehavior(TaskSet):
+#
+#     @task
+#     def get_airport_correct(self):
+#         self.client.get("/v0.1/airport?airport_name=Heathrow")
+#
+#     @task
+#     def get_airport_no_entries(self):
+#         self.client.get("/v0.1/airport?airport_name=kjsjdks")
+#
+#     @task
+#     def get_airport_long_result(self):
+#         self.client.get("/v0.1/airport?airport_name=a")
+#
+#     @task
+#     def get_airport_invalid(self):
+#         self.client.get("/v0.1/airport?airport_name=")
+#
+#
+# class WebsiteUser(HttpUser):
+#     task_set = UserBehavior
+#     host = 'http://127.0.0.1'
+#     min_wait = 5000
+#     max_wait = 9000
