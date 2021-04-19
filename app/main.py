ce0ba03394f372fef6384744dd23ed227fc71de8
Ash <soccer1996ash@yahoo.com.sg>
2021-04-19 16:32:53 +0800

    authorization tutorial done

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from fastapi import Depends, FastAPI, HTTPException, status
 from routers import airports
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from admin import user
+from admin import user, tokens
+from passlib.context import CryptContext
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+from typing import Optional
+
+
+# SECRET_KEY = "c813a22e1e75625a5dc337ba870fa65bee693d5dd946685b913d35fc8a571e84"
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
 fake_users_db = {
@@ -9,16 +19,10 @@ fake_users_db = {
         "username": "johndoe",
         "full_name": "John Doe",
         "email": "johndoe@example.com",
-        "hashed_password": "fakehashedsecret",
+        "hashed_password":
+            "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
         "disabled": False,
-    },
-    "alice": {
-        "username": "alice",
-        "full_name": "Alice Wonderson",
-        "email": "alice@example.com",
-        "hashed_password": "fakehashedsecret2",
-        "disabled": True,
-    },
+    }
 }
 
 
@@ -29,10 +33,13 @@ app = FastAPI(
 )
 
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-
 app.include_router(airports.router)
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
 @app.get("/")
@@ -40,8 +47,12 @@ def root(token: str = Depends(oauth2_scheme)):
     return {"message": "Welcome to the Landing Page!"}
 
 
-def fake_hash_password(password: str):
-    return "fakehashed" + password
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
 
 
 def get_user(db, username: str):
@@ -50,32 +61,60 @@ def get_user(db, username: str):
         return user.UserInDB(**user_dict)
 
 
-def fake_decode_token(token):
-    curruser = get_user(fake_users_db, token)
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def authenticate_user(fake_db, username: str, password: str):
+    curruser = get_user(fake_db, username)
+    if not curruser:
+        return False
+    if not verify_password(password, curruser.hashed_password):
+        return False
     return curruser
 
 
 @app.post("/token")
 async def login(form_data: OAuth2PasswordRequestForm = Depends()):
-    user_dict = fake_users_db.get(form_data.username)
-    if not user_dict:
-        raise HTTPException(status_code=400, detail="Incorrect user/password")
-    curruser = user.UserInDB(**user_dict)
-    hashed_password = fake_hash_password(form_data.password)
-    if not hashed_password == curruser.hashed_password:
-        raise HTTPException(status_code=400, detail="Incorrect user/password")
-
-    return {"access_token": curruser.username, "token_type": "bearer"}
-
-
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    curruser = fake_decode_token(token)
+    curruser = authenticate_user(fake_users_db, form_data.username,
+                                 form_data.password)
     if not curruser:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid authentication credentials",
+            detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": curruser.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = tokens.TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    curruser = get_user(fake_users_db, username=token_data.username)
+    if curruser is None:
+        raise credentials_exception
     return curruser
 
 
